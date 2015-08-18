6bfb25003887eb59b5b51080d8b8bf28554d0869
geotheory <geotheory1@gmail.com>
2015-08-18 23:10:49 +0100

    config

--- get_odk_data.sh
+++ get_odk_data.sh
@@ -2,8 +2,10 @@
 
 dir=$(pwd)'/data'
 
+. config
+
 # extract XML from Aggregate with Briefcase
-java -jar ODK_Briefcase_v1.4.5_Production.jar --form_id MOAS_OPD_v.8.2a --storage_directory $dir --aggregate_url http://14.141.58.198:8080/ODKAggregate --odk_username Pete --odk_password MASTERS > $dir'/debug.log' 2>&1;
+java -jar ODK_Briefcase_v1.4.5_Production.jar --form_id MOAS_OPD_v.8.2a --storage_directory $dir --aggregate_url $IP --odk_username $aggUser --odk_password $aggPwd > $dir'/debug.log' 2>&1;
 
 # convert to csv
 java -jar ODK_Briefcase_v1.4.5_Production.jar --form_id MOAS_OPD_v.8.2a --storage_directory $dir --export_directory $dir --export_filename odk_data.csv > $dir'/debug.log' 2>&1;
