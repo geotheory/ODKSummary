2592a3fa2a0c9a264fa368a342abb712ea25954f
robinedwards <geotheory1@gmail.com>
2016-04-14 22:20:41 +0100

    moas2 trial deployment

--- get_odk_data.sh
+++ get_odk_data.sh
@@ -8,6 +8,8 @@ su -c ". /home/postgres/export_data.sh" -s /bin/sh postgres
 
 cp /home/postgres/processed*.csv /var/lib/tomcat6/webapps/ODKSummary/data/
 
+cp /home/postgres/processed*.csv /home/edison/sd/
+
 cd /var/lib/tomcat6/webapps/ODKSummary
 
 python csv2json.py data/processed_rescued_people.csv data/processed_rescued_people.json
